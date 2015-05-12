3e18bb3b38413a43409bce27274145c45b9a1cdf
amine <yaiche.amin@gmail.com>
2015-05-12 11:28:10 +0100

    add DAO layer to category managed bean

--- src/java/beans/Category.java
+++ src/java/beans/Category.java
@@ -7,7 +7,12 @@ package beans;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
@@ -23,7 +28,7 @@ public class Category {
     private final static String USER = "root";
     private final static String PWD = "";
     private Connection cnx = null;
-    private Statement stat = null;
+    private PreparedStatement stat = null;
     
     private Long id;
     private String label;
@@ -33,7 +38,6 @@ public class Category {
         try{
             Class.forName("com.mysql.jdbc.Driver");
             cnx = DriverManager.getConnection(URL, USER, PWD);
-            stat = cnx.createStatement();
         }catch(Exception e)
         {
             e.printStackTrace();
@@ -57,7 +61,77 @@ public class Category {
     }
     
 //-------------------------------DAO----------------------------------------
+    
+     public void addCategory(Category c){ //NOT YET TESTED
+        String req = "INSERT INTO category VALUES( null , '?');";
+        try {
+            stat = cnx.prepareStatement(req);
+            stat.setString(1, c.label);
+            
+            stat.executeUpdate();
+            
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public void deleteCategory(Category c){ //NOT YET TESTED
+        String req = "DELETE FROM category WHERE  1 ";
+            req += (c.id != null)? "AND id = "+ c.id : "";
+            req += (c.label != null)? "AND label = '"+c.label+"' " : "";
+            req += ";";
+            
+        try{
+            stat = cnx.prepareStatement(req);
+            stat.executeUpdate();
+            
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+    
+    public List<Category> getAllCategory(){ // NOT YET TESTED
+        String req = "SELECT * FROM category;";
+        List<Category> l = new ArrayList<Category>();
+        
+        try{
+            stat = cnx.prepareStatement(req);
+            ResultSet result = stat.executeQuery();
+            
+            while(result.next()){
+                Category c = new Category();
+                fillCategory(c,result);
+                l.add(c);
+            }    
+        }catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+        
+        return l;   
+    }
+    
+    private void fillCategory(Category c , ResultSet result)throws SQLException{ //NOT YET TESTED
+        c.id = result.getLong("id");
+        c.label = result.getString("label");
+    }
+    
+    public void updateCategory(Category c){//NOT YET TESTED
+        String req = "UPDATE category c SET label = ? WHERE id = ?";
+        
+        try{
+            stat = cnx.prepareStatement(req);
+            stat.setString(1, c.label);
+            stat.setLong(2, c.id);
+            
+            stat.executeQuery();
+            
+            
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        
+    }
+            
 
-    
-    
 }
