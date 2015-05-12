293d30983546a050ec06ddfd7c5ee302c65926c0
amine <yaiche.amin@gmail.com>
2015-05-12 11:06:25 +0100

    add DAO layer to message managed bean

--- src/java/beans/Message.java
+++ src/java/beans/Message.java
@@ -6,11 +6,15 @@
 package beans;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-import sun.util.calendar.BaseCalendar;
 
 /**
  *
@@ -24,12 +28,12 @@ public class Message {
     private final static String USER = "root";
     private final static String PWD = "";
     private Connection cnx = null;
-    private Statement stat = null;
+    private PreparedStatement stat = null;
     
     
     private Long id;
     private String msg;
-    private BaseCalendar.Date date;
+    private Date date;
     private User sender;
     private User receiver;
     
@@ -38,7 +42,6 @@ public class Message {
         try{
             Class.forName("com.mysql.jdbc.Driver");
             cnx = DriverManager.getConnection(URL, USER, PWD);
-            stat = cnx.createStatement();
         }catch(Exception e)
         {
             e.printStackTrace();
@@ -53,7 +56,7 @@ public class Message {
         return msg;
     }
 
-    public BaseCalendar.Date getDate() {
+    public Date getDate() {
         return date;
     }
 
@@ -81,10 +84,78 @@ public class Message {
         this.receiver = receiver;
     }
 
-    public void setDate(BaseCalendar.Date date) {
+    public void setDate(Date date) {
         this.date = date;
     }
     
 //-------------------------------DAO----------------------------------------
+    
+    public void addMessage(Message m){ //NOT YET TESTED
+        String req = "INSERT INTO message VALUES( null , '?', NOW(), ?, '?');";
+        try {
+            stat = cnx.prepareStatement(req);
+            stat.setString(1, m.msg);
+            stat.setLong(2,m.sender.getId());
+            stat.setLong(3,m.receiver.getId());
+            
+            stat.executeUpdate();
+            
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public void deleteMessage(Message m){ //NOT YET TESTED
+        String req = "DELETE FROM message WHERE  1 ";
+            req += (m.id != null)? "AND id = "+ m.id : "";
+            req += (m.receiver.getLogin() != null)? "AND    receiver = '"+m.receiver.getLogin()+"' " : "";
+            req += (m.sender.getLogin() != null)? "AND    sender = '"+m.sender.getLogin()+"' " : "";
+            
+        
+        try{
+            stat = cnx.prepareStatement(req);
+            stat.executeUpdate();
+            
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+    
+     
+    public List<Message> getMessage(Long messageId , User sender, User receiver){ //NOT YET TESTED
+        String req = "SELECT * FROM message WHERE 1 ";
+            req += (messageId != null)?" AND id = "+messageId :"";
+            req += (sender != null)? " AND sender = "+sender.getId() : "";
+            req += (receiver != null)? " AND receiver = "+receiver.getId() : "";
+            req += ";";
+            
+        try{
+            List<Message> l = new ArrayList<Message>();
+            stat = cnx.prepareStatement(req);
+            ResultSet result = stat.executeQuery();
+            
+            while(result.next()){
+                Message m = new Message();
+                fillMessage(m,result);
+                
+                l.add(m);
+                return l;
+            }
+            
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return null;            
+    }
+    
+    private void fillMessage(Message m , ResultSet result)throws SQLException{ //NOT YET TESTED
+        m.msg = result.getString("msg");
+        m.date=result.getDate("date");
+ //NOT YET       m.sender = result.getLong("sender");
+//NOT YET        m.receiver = result.getLong("receiver");
+    }
+    
+    
+    
     
 }
